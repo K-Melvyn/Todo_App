981b17cbe83a1125f980a6cf631f18b029768541
Melvyn <tmelvyn32@gmail.com>
2025-03-19 09:24:00 +0000

    Ajout d'un nouveau fichier

--- mail.php
+++ mail.php
@@ -4,14 +4,14 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-function  sendVerificationEmail($email, $otp) {
+function sendVerificationEmail($email, $otp) {
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host       = 'smtp.example.com'; // Your SMTP server
+        $mail->Host       = 'smtp.gmail.com'; // Correct SMTP server for Gmail
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'tmelvyn32@gmail.com'; // Your SMTP username
-        $mail->Password   = 'jsge mams xyzz quma'; // Your SMTP password
+        $mail->Username   = 'tmelvyn32@gmail.com'; // Your Gmail
+        $mail->Password   = 'jsge mams xyzz quma'; // Your Gmail App Password (NOT your real password)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
@@ -21,18 +21,14 @@ function  sendVerificationEmail($email, $otp) {
         $mail->Subject = 'Verify Your Email';
         $mail->Body    = "Your OTP is: <b>$otp</b>";
 
-        $mail->send();
+        $mail->send(); // Send email
 
-        if (!$mail->send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            echo "Email send succesfully !";
-        }
-        
-
+        echo "Email sent successfully!";
         return true;
     } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
         return false;
     }
 }
+
 ?>
